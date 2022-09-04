8d607bac74c75a0cbbdbd592e4f22713434f0989
Christopher Holmok <christopher@holmok.com>
2022-09-04 13:48:37 -0500

    remix dir

--- remix-app/remix.config.js
+++ remix-app/remix.config.js
@@ -1,9 +1,8 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  async routes(defineRoutes) {
-    return defineRoutes((route) => {
-      route("/remix/*", "index.tsx");
-    });
-  }
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+   publicPath: "/remix/build/",
 };
