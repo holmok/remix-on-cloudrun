16a7054bc7bbff931769233d2ec887e615431755
Christopher Holmok <christopher@holmok.com>
2022-09-04 13:45:24 -0500

    remix dir

--- remix-app/app/routes/remix.tsx
+++
@@ -1,8 +0,0 @@
-export default function Remix() {
-  return (
-    <>
-   <h1>Hey, It's Remix!</h1>
-   <p>This is getting served from the remix app.</p>
-   </>
-  );
-}
--- remix-app/remix.config.js
+++ remix-app/remix.config.js
@@ -1,8 +1,9 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ["**/.*"],
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
+  async routes(defineRoutes) {
+    return defineRoutes((route) => {
+      route("/remix/*", "index.tsx");
+    });
+  }
 };
