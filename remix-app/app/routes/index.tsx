6ef432c4f02faedcf39448369963bdcbc63e55ae
Christopher Holmok <holmok@users.noreply.github.com>
2022-09-06 15:32:27 -0500

    no mo ?

--- remix-app/app/routes/index.tsx
+++ remix-app/app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   console.log(`page request`);
   return (
     <>
-   <h1>Hey, It's Remix?!</h1>
+   <h1>Hey, It's Remix!</h1>
    <p>This is getting served from the remix app, and is running on cloud run.</p>
    </>
   );
