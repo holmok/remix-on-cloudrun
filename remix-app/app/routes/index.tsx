5d72062e2ef0cac79c3c8e693eb31d1459a65d74
Christopher Holmok <holmok@users.noreply.github.com>
2022-09-30 14:47:02 -0500

    example

--- remix-app/app/routes/index.tsx
+++ remix-app/app/routes/index.tsx
@@ -3,7 +3,7 @@ export default function Index() {
   return (
     <>
    <h1>Hey, It's Remix!</h1>
-   <p>This is getting served from the remix app, and is running on <strong>CloudRun</strong>.</p>
+   <p>This is getting served from the remix app, and is running on CloudRun!</p>
    </>
   );
 }
